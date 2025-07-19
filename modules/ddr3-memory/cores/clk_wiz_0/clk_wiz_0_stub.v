464c28e24be93bfc2cb02583761d0b442ef9c200
JM Sudar <code.jms@gmail.com>
2025-07-19 14:46:19 -0700

    added ddr3 module

---
+++ modules/ddr3-memory/cores/clk_wiz_0/clk_wiz_0_stub.v
@@ -0,0 +1,27 @@
+// Copyright 1986-2022 Xilinx, Inc. All Rights Reserved.
+// Copyright 2022-2024 Advanced Micro Devices, Inc. All Rights Reserved.
+// --------------------------------------------------------------------------------
+// Tool Version: Vivado v.2024.2 (win64) Build 5239630 Fri Nov 08 22:35:27 MST 2024
+// Date        : Tue Jul 15 17:27:41 2025
+// Host        : DESKTOP-MBJU2VD running 64-bit major release  (build 9200)
+// Command     : write_verilog -force -mode synth_stub
+//               c:/Users/JMSudar/Code/alchitry-learning-module/modules/ddr3-memory/cores/clk_wiz_0/clk_wiz_0_stub.v
+// Design      : clk_wiz_0
+// Purpose     : Stub declaration of top-level module interface
+// Device      : xc7a35tftg256-2
+// --------------------------------------------------------------------------------
+
+// This empty module with port declaration file causes synthesis tools to infer a black box for IP.
+// The synthesis directives are for Synopsys Synplify support to prevent IO buffer insertion.
+// Please paste the declaration into a Verilog source file or add the file as an additional source.
+(* CORE_GENERATION_INFO = "clk_wiz_0,clk_wiz_v6_0_15_0_0,{component_name=clk_wiz_0,use_phase_alignment=true,use_min_o_jitter=false,use_max_i_jitter=false,use_dyn_phase_shift=false,use_inclk_switchover=false,use_dyn_reconfig=false,enable_axi=0,feedback_source=FDBK_AUTO,PRIMITIVE=MMCM,num_out_clk=2,clkin1_period=10.000,clkin2_period=10.000,use_power_down=false,use_reset=true,use_locked=true,use_inclk_stopped=false,feedback_type=SINGLE,CLOCK_MGR_TYPE=NA,manual_override=false}" *) 
+module clk_wiz_0(clk_100, clk_200, resetn, locked, clk_in)
+/* synthesis syn_black_box black_box_pad_pin="resetn,locked,clk_in" */
+/* synthesis syn_force_seq_prim="clk_100" */
+/* synthesis syn_force_seq_prim="clk_200" */;
+  output clk_100 /* synthesis syn_isclock = 1 */;
+  output clk_200 /* synthesis syn_isclock = 1 */;
+  input resetn;
+  output locked;
+  input clk_in;
+endmodule
---
+++ modules/ddr3-memory/cores/clk_wiz_0/clk_wiz_0_stub.vhdl
@@ -0,0 +1,35 @@
+-- Copyright 1986-2022 Xilinx, Inc. All Rights Reserved.
+-- Copyright 2022-2024 Advanced Micro Devices, Inc. All Rights Reserved.
+-- --------------------------------------------------------------------------------
+-- Tool Version: Vivado v.2024.2 (win64) Build 5239630 Fri Nov 08 22:35:27 MST 2024
+-- Date        : Tue Jul 15 17:27:41 2025
+-- Host        : DESKTOP-MBJU2VD running 64-bit major release  (build 9200)
+-- Command     : write_vhdl -force -mode synth_stub
+--               c:/Users/JMSudar/Code/alchitry-learning-module/modules/ddr3-memory/cores/clk_wiz_0/clk_wiz_0_stub.vhdl
+-- Design      : clk_wiz_0
+-- Purpose     : Stub declaration of top-level module interface
+-- Device      : xc7a35tftg256-2
+-- --------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity clk_wiz_0 is
+  Port ( 
+    clk_100 : out STD_LOGIC;
+    clk_200 : out STD_LOGIC;
+    resetn : in STD_LOGIC;
+    locked : out STD_LOGIC;
+    clk_in : in STD_LOGIC
+  );
+
+  attribute CORE_GENERATION_INFO : string;
+  attribute CORE_GENERATION_INFO of clk_wiz_0 : entity is "clk_wiz_0,clk_wiz_v6_0_15_0_0,{component_name=clk_wiz_0,use_phase_alignment=true,use_min_o_jitter=false,use_max_i_jitter=false,use_dyn_phase_shift=false,use_inclk_switchover=false,use_dyn_reconfig=false,enable_axi=0,feedback_source=FDBK_AUTO,PRIMITIVE=MMCM,num_out_clk=2,clkin1_period=10.000,clkin2_period=10.000,use_power_down=false,use_reset=true,use_locked=true,use_inclk_stopped=false,feedback_type=SINGLE,CLOCK_MGR_TYPE=NA,manual_override=false}";
+end clk_wiz_0;
+
+architecture stub of clk_wiz_0 is
+  attribute syn_black_box : boolean;
+  attribute black_box_pad_pin : string;
+  attribute syn_black_box of stub : architecture is true;
+  attribute black_box_pad_pin of stub : architecture is "clk_100,clk_200,resetn,locked,clk_in";
+begin
+end;
